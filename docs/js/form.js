45c6982ee29704cf16926daba7e81bc81d009745
Steffen Meuser <info@me-user.de>
2020-11-09 20:00:29 +0100

    Weiche für Aufruf aus Webserver und als Dokument (git)

---
+++ docs/js/form.js
@@ -0,0 +1,51 @@
+var submitForm = function(url) {
+  var form = document.querySelector('form');
+  form.action = url;
+  form.submit();
+}
+
+var fetchGetParams = function(string) {
+  var raw = string.split('&');
+  var params = {};
+  raw.forEach((param) => {
+    var value = param.split('=');
+    params[value[0]] = value[1];
+  });
+
+  return params;
+}
+
+window.addEventListener('load', function(event) {
+  var requestPath = '4DAction/api_put_project';
+  var params = fetchGetParams(window.location.search.substr(1));
+  var form = document.querySelector('form');
+
+  if (params['token'] != undefined) {
+    document.querySelector('input[name=token]').value = params['token'];
+  }
+
+  if (window.location.protocol == 'http:' || window.location.protocol == 'https:') {
+    var host = document.querySelector('input[name=host]');
+    if (host) {
+      host.parentElement.parentElement.remove();
+    }
+  }
+
+  var hostInput = document.querySelector('input[name=host]');
+  if (hostInput && params['host'] && params['host'].match(/(^http(s?)):\/\/(.*)\//)) {
+    hostInput.value = params['host'];
+    form.action = params['host'] + '/' + requestPath;
+  } else {
+    var origin = window.location.origin;
+    form.action = origin + '/' + requestPath;
+  }
+
+  form.addEventListener('submit', (event) => {
+    if (hostInput) {
+      var hostValue = hostInput.value;
+      if (hostValue && hostValue.match(/(^http(s?)):\/\/(.*)\//)) {
+        form.action = hostValue + requestPath;
+      }
+    }
+  });
+});
